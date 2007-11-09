30dca2c6794b45128607766f4cc7ad89e0522869
Mikel Larreategi <mlarreategi@codesyntax.com>
2007-11-09 15:51:59 +0000

    Show the native language name, and show only other languages than selected

--- cs/i18n/browser/selector.py
+++ cs/i18n/browser/selector.py
@@ -3,3 +3,32 @@ from plone.app.i18n.locales.browser.selector import LanguageSelector as LSBase
 
 class LanguageSelector(LSBase):
     render = ZopeTwoPageTemplateFile('templates/languageselector.pt')
+
+
+    def languages(self):
+        """ Returns list of languages """
+
+        if self.tool is None:
+            return []
+
+        bound = self.tool.getLanguageBindings()
+        current = bound[0]
+
+        supported = self.tool.getSupportedLanguages()
+
+        lang_info = self.tool.getAvailableLanguageInformation()
+        languages = []
+        for lang in supported:
+            data = {}
+            data['code'] = lang
+            if lang == current:
+                data['selected'] = True
+            else:
+                data['selected'] = False
+            data['flag'] = lang_info.get(lang).get(u'flag', None)
+            data['name'] = lang_info.get(lang).get(u'name', None)
+            data['native'] = lang_info.get(lang).get(u'native', None)
+            
+            languages.append(data)
+
+        return languages
