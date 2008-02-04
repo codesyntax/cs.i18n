bf1058889c3212f5b78396e654262f7b6b2194f6
Mikel Larreategi <mlarreategi@codesyntax.com>
2008-02-04 09:34:23 +0000

    changes not to show the current language and show the language list ordered

--- cs/i18n/browser/translatable.py
+++ cs/i18n/browser/translatable.py
@@ -2,6 +2,9 @@ from Products.LinguaPlone.browser.selector import TranslatableLanguageSelector a
 
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 
+from zope.i18n.interfaces import IUserPreferredLanguages
+from plone.memoize.view import memoize
+
 class TranslatableLanguageSelector(TLSBase):
     render = ZopeTwoPageTemplateFile('templates/translatablelanguageselector.pt')
     def languages(self):
@@ -10,6 +13,10 @@ class TranslatableLanguageSelector(TLSBase):
         lang_info = self.tool.getAvailableLanguageInformation()
 
         for data in results:
-            data['native'] = lang_info.get(data['code']).get(u'native', None)
+            data['native'] = lang_info.get(data['code']).get(u'native', None).strip()
 
-        return results
+        return sorted(results, lambda x,y:cmp(x['native'],y['native']))
+
+    @memoize
+    def getPreferredLanguage(self):
+        return IUserPreferredLanguages(self.request).getPreferredLanguages()[0]
