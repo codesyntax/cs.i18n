e6e64aa9529e7a6c06358cfb301e3bf2f030d49f
Mikel Larreategi <mlarreategi@codesyntax.com>
2008-02-05 11:25:23 +0000

    selected is correctly exported

--- cs/i18n/browser/translatable.py
+++ cs/i18n/browser/translatable.py
@@ -3,20 +3,12 @@ from Products.LinguaPlone.browser.selector import TranslatableLanguageSelector a
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 
 from zope.i18n.interfaces import IUserPreferredLanguages
-from plone.memoize.view import memoize
 
 class TranslatableLanguageSelector(TLSBase):
     render = ZopeTwoPageTemplateFile('templates/translatablelanguageselector.pt')
+
     def languages(self):
-        """ Returns list of languages """
+        """ Return languages sorted according to their native name """
         results = TLSBase.languages(self)
-        lang_info = self.tool.getAvailableLanguageInformation()
-
-        for data in results:
-            data['native'] = lang_info.get(data['code']).get(u'native', None).strip()
-
         return sorted(results, lambda x,y:cmp(x['native'],y['native']))
 
-    @memoize
-    def getPreferredLanguage(self):
-        return IUserPreferredLanguages(self.request).getPreferredLanguages()[0]
