14ed7068e35b99441b710e70bbaf3826ecf85f1a
Mikel Larreategi <mlarreategi@codesyntax.com>
2008-04-29 14:20:50 +0000

    as LinguaPlone did, our viewlet will handle everything

--- cs/i18n/browser/translatable.py
+++ cs/i18n/browser/translatable.py
@@ -1,14 +1,17 @@
-from Products.LinguaPlone.browser.selector import TranslatableLanguageSelector as TLSBase
+try:
+    from Products.LinguaPlone.browser.selector import TranslatableLanguageSelector as LSBase
+except ImportError:
+    from plone.app.i18n.locales.browser.selector import LanguageSelector as LSBase
 
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 
 from zope.i18n.interfaces import IUserPreferredLanguages
 
-class TranslatableLanguageSelector(TLSBase):
+class TranslatableLanguageSelector(LSBase):
     render = ZopeTwoPageTemplateFile('templates/translatablelanguageselector.pt')
 
     def languages(self):
         """ Return languages sorted according to their native name """
-        results = TLSBase.languages(self)
+        results = LSBase.languages(self)
         return sorted(results, lambda x,y:cmp(x['native'],y['native']))
 
